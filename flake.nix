9f56c69dc2fc257a52f3fa6e5b897174bc4eb406
Jared Baur <jaredbaur@fastmail.com>
2022-09-12 18:27:55 -0700

    Add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   description = "webauthn-tiny";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    pre-commit.url = "github:cachix/pre-commit-hooks.nix";
   };
   outputs = inputs: with inputs; {
     overlays.default = _: prev: {
@@ -11,12 +12,23 @@
   } // flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
-        inherit system; overlays = [ self.overlays.default ];
+        inherit system;
+        overlays = [ self.overlays.default ];
+      };
+      pre-commit-hooks = pre-commit.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          cargo-check.enable = true;
+          clippy.enable = true;
+          nixpkgs-fmt.enable = true;
+          rustfmt.enable = true;
+        };
       };
     in
     {
       packages.default = pkgs.webauthn-tiny;
       devShells.default = pkgs.mkShell {
+        inherit (pre-commit-hooks) shellHook;
         inherit (pkgs.webauthn-tiny)
           PKG_CONFIG_PATH
           nativeBuildInputs;
