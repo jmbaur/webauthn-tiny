58710626be79af2af2f34004460397096d58b5f4
Jared Baur <jaredbaur@fastmail.com>
2022-09-12 20:00:57 -0700

    Add register handler

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,24 @@ use argon2::{
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Extension};
 use axum_auth::AuthBasic;
 use clap::{arg, Command};
+use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, fs, io::ErrorKind, sync::Arc};
+use webauthn_rs::{
+    prelude::{Url, Uuid},
+    WebauthnBuilder,
+};
 
-type UserPass = HashMap<String, String>;
+#[derive(Deserialize, Serialize)]
+struct UserState {
+    id: Uuid,
+    hash: String,
+}
+type UserMap = HashMap<String, UserState>;
 
 struct AppState {
-    passwords: UserPass,
+    users: UserMap,
+    origin: String,
+    id: String,
 }
 
 fn adduser(sub_m: &clap::ArgMatches) -> anyhow::Result<()> {
@@ -31,7 +43,7 @@ fn adduser(sub_m: &clap::ArgMatches) -> anyhow::Result<()> {
             e => return Err(anyhow::anyhow!(e)),
         },
     };
-    let mut passwords = serde_yaml::from_str::<UserPass>(&password_file_contents)?;
+    let mut users = serde_yaml::from_str::<UserMap>(&password_file_contents)?;
 
     let argon = Argon2::default();
     let salt = SaltString::generate(&mut OsRng);
@@ -41,73 +53,86 @@ fn adduser(sub_m: &clap::ArgMatches) -> anyhow::Result<()> {
         Err(e) => return Err(anyhow::anyhow!(e)),
     };
 
-    if let Some(current) = passwords.get_mut(username) {
-        *current = hash.to_string();
+    if let Some(user) = users.get_mut(username) {
+        user.hash = hash.to_string();
     } else {
-        passwords.insert(username.to_string(), hash.to_string());
+        users.insert(
+            username.to_string(),
+            UserState {
+                id: Uuid::new_v4(),
+                hash: hash.to_string(),
+            },
+        );
     }
 
-    let updated_password_file_contents = serde_yaml::to_string(&passwords)?;
+    let updated_password_file_contents = serde_yaml::to_string(&users)?;
 
     fs::write(password_file, updated_password_file_contents)?;
 
     Ok(())
 }
 
-async fn register_handler() -> impl IntoResponse {
-    // let rp_id = "localhost";
-    // let rp_origin = Url::parse("http://localhost:8080").expect("Invalid URL");
-    // let webauthn = WebauthnBuilder::new(rp_id, &rp_origin)
-    //     .expect("Invalid configuration")
-    //     .allow_subdomains(false)
-    //     .build()
-    //     .expect("Invalid configuration");
-    //
-    // let uuid = Uuid::new_v4();
-    // let username = "jared";
-    // let userdisplayname = "Jared Baur";
-    // let (chal, _) = webauthn
-    //     .start_passkey_registration(uuid, username, userdisplayname, None)
-    //     .expect("Failed to start registration");
-    //
-    // let chal_json = serde_json::to_string(&chal).expect("Failed to serialize public key challenge");
+fn passwords_match(password: Option<String>, hash: String) -> bool {
+    if password.is_none() {
+        return false;
+    }
 
-    // let parsed_hash = PasswordHash::new(&password_hash)?;
-    // assert!(Argon2::default().verify_password(password, &parsed_hash).is_ok());
+    let parsed_hash = match PasswordHash::new(&hash) {
+        Ok(p) => p,
+        Err(_) => return false,
+    };
 
-    StatusCode::OK
+    let argon = Argon2::default();
+    argon
+        .verify_password(
+            password.expect("already checked is not none").as_bytes(),
+            &parsed_hash,
+        )
+        .is_ok()
+}
+async fn register_handler(
+    AuthBasic((username, password)): AuthBasic,
+    Extension(state): Extension<Arc<AppState>>,
+) -> impl IntoResponse {
+    let maybe_user = state.users.get(&username);
+    if maybe_user.is_none() {
+        return (StatusCode::UNAUTHORIZED, String::from("{}"));
+    }
+    let user = maybe_user.expect("already checked is not none");
+
+    if !passwords_match(password, user.hash.to_owned()) {
+        return (StatusCode::UNAUTHORIZED, String::from("{}"));
+    }
+
+    let rp_id = &state.id;
+    let rp_origin = Url::parse(state.origin.as_str()).expect("Invalid URL");
+    let webauthn = WebauthnBuilder::new(rp_id.as_str(), &rp_origin)
+        .expect("Invalid configuration")
+        .allow_subdomains(false)
+        .build()
+        .expect("Invalid configuration");
+
+    let (chal, _) = webauthn
+        .start_passkey_registration(user.id, username.as_str(), username.as_str(), None)
+        .expect("Failed to start registration");
+
+    (
+        StatusCode::OK,
+        serde_json::to_string(&chal).expect("Failed to serialize public key challenge"),
+    )
 }
 
 async fn auth_handler(
     AuthBasic((username, password)): AuthBasic,
     Extension(state): Extension<Arc<AppState>>,
 ) -> impl IntoResponse {
-    if password.is_none() {
-        return StatusCode::UNAUTHORIZED;
+    match state.users.get(&username) {
+        Some(user) => match passwords_match(password, user.hash.to_owned()) {
+            true => StatusCode::OK,
+            false => StatusCode::UNAUTHORIZED,
+        },
+        None => StatusCode::UNAUTHORIZED,
     }
-
-    let password_hash = match state.passwords.get(&username) {
-        Some(p) => p,
-        None => return StatusCode::UNAUTHORIZED,
-    };
-
-    let parsed_hash = match PasswordHash::new(&password_hash) {
-        Ok(p) => p,
-        Err(_) => return StatusCode::UNAUTHORIZED,
-    };
-
-    let argon = Argon2::default();
-    if !argon
-        .verify_password(
-            password.expect("already checked is not none").as_bytes(),
-            &parsed_hash,
-        )
-        .is_ok()
-    {
-        return StatusCode::UNAUTHORIZED;
-    }
-
-    StatusCode::OK
 }
 
 async fn serve(sub_m: &clap::ArgMatches) -> anyhow::Result<()> {
@@ -116,7 +141,18 @@ async fn serve(sub_m: &clap::ArgMatches) -> anyhow::Result<()> {
         .expect("`passwordfile` is required");
     let password_file_contents = fs::read_to_string(password_file)?;
     let passwords = serde_yaml::from_str(&password_file_contents)?;
-    let app_state = Arc::new(AppState { passwords });
+
+    let origin = sub_m
+        .get_one::<String>("origin")
+        .expect("`origin` is required");
+
+    let id = sub_m.get_one::<String>("id").expect("`id` is required");
+
+    let app_state = Arc::new(AppState {
+        users: passwords,
+        origin: origin.to_string(),
+        id: id.to_string(),
+    });
 
     let default_socket_addr = &"[::]:8080".to_string();
     let socket_addr: &String = match sub_m.get_one("address") {
@@ -144,13 +180,15 @@ async fn main() -> anyhow::Result<()> {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(Command::new("adduser").args(&[
-            arg!(-u --username <NAME> "The name for the user"),
-            arg!(-p --password <PASSWORD> "The password for the user"),
             arg!(-f --passwordfile <FILE> "The path to a password file"),
+            arg!(-p --password <PASSWORD> "The password for the user"),
+            arg!(-u --username <NAME> "The name for the user"),
         ]))
         .subcommand(Command::new("serve").args(&[
             arg!(-a --address [ADDRESS] "Socket address to bind to"),
             arg!(-f --passwordfile <FILE> "The path to a password file"),
+            arg!(-i --id [ID] "ID to use for webauthn"),
+            arg!(-o --origin [ORIGIN] "Origin to use for webauthn"),
         ]))
         .get_matches();
 
