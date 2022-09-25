56d2083f74a7968ea3b617ae6f7710786c586d9b
Jared Baur <jaredbaur@fastmail.com>
2022-09-25 03:45:18 +0000

    Specify browser targets

--- webui-build.bash
+++ webui-build.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-esbuild index.tsx --bundle --minify --sourcemap --target=esnext --outfile=dist/index.js
+esbuild index.tsx --bundle --minify --sourcemap --target=es2020,chrome58,edge18,firefox57,node12,safari11 --outfile=dist/index.js
 cp index.html dist/index.html
 cp favicon.ico dist/favicon.ico
