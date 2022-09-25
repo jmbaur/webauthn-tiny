257ae40d18fb2e6d74bdece7097ad6602b61dfbf
Jared Baur <jaredbaur@fastmail.com>
2022-09-24 23:54:05 -0700

    Don't use global basic auth

--- module.nix
+++ module.nix
@@ -70,17 +70,18 @@ in
           };
         in
         {
-          inherit (cfg.nginx) basicAuthFile basicAuth;
           locations."= ${cfg.nginx.basePath}/api/validate" = withProxy {
             extraConfig = ''
-              auth_basic off;
               proxy_set_header Host            $host;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header X-Remote-User   $remote_user;
             '';
           };
-          locations."${cfg.nginx.basePath}/api" = withProxy { };
-          locations.${cfg.nginx.basePath} = {
+          locations."${cfg.nginx.basePath}/api" = withProxy {
+            inherit (cfg.nginx) basicAuthFile basicAuth;
+          };
+          locations."${cfg.nginx.basePath}/" = {
+            inherit (cfg.nginx) basicAuthFile basicAuth;
             alias = "${pkgs.webauthn-tiny.web-ui}/";
             tryFiles = "$uri /index.html =404";
           };
