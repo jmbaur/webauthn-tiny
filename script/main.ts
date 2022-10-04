e586e162591bb5ae5291a51b8ecc4f3ffab39ff1
Jared Baur <jaredbaur@fastmail.com>
2022-10-04 13:52:04 -0700

    Modify script

--- script/main.ts
+++ script/main.ts
@@ -8,59 +8,7 @@ import {
   parseRequestOptionsFromJSON,
 } from "https://esm.sh/@github/webauthn-json@2.0.1/browser-ponyfill.js?target=deno";
 
-const deleteButton = document.getElementById("delete-button");
-if (deleteButton) {
-  deleteButton.addEventListener("click", async function (ev: MouseEvent) {
-    ev.preventDefault(); // TODO(jared): not needed?
-    const cred_id = deleteButton.getAttribute("value");
-    if (cred_id) {
-      try {
-        const response = await fetch(`/api/credentials/${cred_id}`, {
-          method: "DELETE",
-        });
-        if (response.status === 204) {
-          location.reload();
-        }
-      } catch (err) {
-        console.error(err);
-        window.alert(err);
-      }
-    }
-  });
-}
-
-const form = document.querySelector("form");
-if (form) {
-  form.addEventListener("submit", async function (ev: SubmitEvent) {
-    ev.preventDefault();
-    const formData = new FormData(form);
-    const newCredential = formData.get("new-cred-name");
-    if (newCredential === null) {
-      window.alert("Name for new credential not found");
-      return;
-    } else if (newCredential === "") {
-      window.alert("Name for new credential is empty");
-      return;
-    }
-    try {
-      const response = await fetch("/api/register", { method: "GET" });
-      const startData: CredentialCreationOptionsJSON = await response.json();
-      const endData = await create(parseCreationOptionsFromJSON(startData));
-      const body = JSON.stringify({ name: newCredential, credential: endData });
-      await fetch("/api/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body,
-      });
-      location.reload();
-    } catch (err) {
-      console.error(err);
-      window.alert(err);
-    }
-  });
-}
-
-export async function startAuthentication(): Promise<CredentialRequestOptions> {
+async function startAuthentication(): Promise<CredentialRequestOptions> {
   const response = await fetch("/api/authenticate", { method: "GET" });
   if (!response.ok) {
     throw new Error("Failed to start authentication");
@@ -74,7 +22,7 @@ export async function startAuthentication(): Promise<CredentialRequestOptions> {
   return parseRequestOptionsFromJSON(data.challenge);
 }
 
-export async function endAuthentication(opts: CredentialRequestOptions) {
+async function endAuthentication(opts: CredentialRequestOptions) {
   const data = await get(opts);
   const body = JSON.stringify(data);
 
@@ -89,3 +37,64 @@ export async function endAuthentication(opts: CredentialRequestOptions) {
     throw new Error("not authenticated");
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const deleteButton = document.getElementById("delete-button");
+  if (deleteButton) {
+    deleteButton.addEventListener("click", async function (ev: MouseEvent) {
+      ev.preventDefault(); // TODO(jared): not needed?
+      const cred_id = deleteButton.getAttribute("value");
+      if (cred_id) {
+        try {
+          const response = await fetch(`/api/credentials/${cred_id}`, {
+            method: "DELETE",
+          });
+          if (response.status === 204) {
+            location.reload();
+          }
+        } catch (err) {
+          console.error(err);
+          window.alert(err);
+        }
+      }
+    });
+  }
+
+  const form = document.querySelector("form");
+  if (form) {
+    form.addEventListener("submit", async function (ev: SubmitEvent) {
+      ev.preventDefault();
+      const formData = new FormData(form);
+      const newCredential = formData.get("new-cred-name");
+      if (newCredential === null) {
+        window.alert("Name for new credential not found");
+        return;
+      } else if (newCredential === "") {
+        window.alert("Name for new credential is empty");
+        return;
+      }
+      try {
+        const response = await fetch("/api/register", { method: "GET" });
+        const startData: CredentialCreationOptionsJSON = await response.json();
+        const endData = await create(parseCreationOptionsFromJSON(startData));
+        const body = JSON.stringify({
+          name: newCredential,
+          credential: endData,
+        });
+        await fetch("/api/register", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body,
+        });
+        location.reload();
+      } catch (err) {
+        console.error(err);
+        window.alert(err);
+      }
+    });
+  }
+
+  if (location.pathname === "/authenticate") {
+    startAuthentication().then(endAuthentication).catch(console.error);
+  }
+});
