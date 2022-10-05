abbc5f9630681bda269df42de76fb99dcc8cf634
Jared Baur <jaredbaur@fastmail.com>
2022-10-05 10:10:05 -0700

    Version bump

--- script/default.nix
+++ script/default.nix
@@ -1,8 +1,11 @@
-{ stdenv, deno2nix, deno, jq, ... }:
-let deps = deno2nix.internal.mkDepsLink ./deno.lock; in
+{ stdenv, deno2nix, deno, jq, lib, ... }:
+let
+  cargoTOML = lib.importTOML ../Cargo.toml;
+  deps = deno2nix.internal.mkDepsLink ./deno.lock;
+in
 stdenv.mkDerivation {
-  pname = "webauthn-tiny-ui";
-  version = "0.1.1";
+  pname = cargoTOML.package.name + "ui";
+  inherit (cargoTOML.package) version;
   src = ./.;
   buildInputs = [ deno jq ];
   configurePhase = ''
