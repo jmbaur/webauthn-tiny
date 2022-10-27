ec53aea37ee84d51d184392083e713803641c39f
Jared Baur <jaredbaur@fastmail.com>
2022-10-27 11:06:11 -0700

    Remove uneeded browser code

--- script/main.ts
+++ script/main.ts
@@ -4,55 +4,30 @@ import {
   parseCreationOptionsFromJSON,
   parseRequestOptionsFromJSON,
 } from "@github/webauthn-json/browser-ponyfill";
-
-async function authenticate() {
-  const startResponse = await fetch("/api/authenticate", { method: "GET" });
-  if (!startResponse.ok) return window.alert("failed to start authentication");
-  if (startResponse.status === 204) location.reload(); // no user credentials
-  const endResponse = await fetch("/api/authenticate", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(
-      await get(
-        parseRequestOptionsFromJSON(await startResponse.json()),
-      ),
-    ),
-  });
-  if (!endResponse.ok) return window.alert("not authenticated");
-  location.replace("/authenticate"); // client is now logged in
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const deleteButtons = document.getElementsByClassName("delete-credential");
-  for (const button of deleteButtons) {
+document.addEventListener("DOMContentLoaded", () => {
+  for (const button of document.getElementsByClassName("delete-credential")) {
     button.addEventListener("click", async function (_: Event) {
       const cred_id = button.getAttribute("value");
-      if (
-        cred_id &&
-        window.confirm("Do you really want to delete this credential?")
-      ) {
+      if (cred_id && window.confirm("Do you want to delete this credential?")) {
         const response = await fetch(`/api/credentials/${cred_id}`, {
           method: "DELETE",
         });
-        if (!response.ok) return window.alert("failed to delete credential");
-        if (response.status === 204) location.reload();
+        if (!response.ok) return window.alert("Failed to delete credential");
+        else if (response.status === 204) return location.reload();
       }
     });
   }
-
   const addButton = document.getElementById("add-credential");
-  if (addButton) {
+  if (addButton != null) {
     addButton.addEventListener("click", async function (_: Event) {
-      const newCredential = window.prompt(
-        "Enter a name for the new credential",
-      );
+      const newCredential = window.prompt("Enter name for the new credential");
       if (newCredential === null) return;
-      if (newCredential === "") {
+      else if (newCredential === "") {
         return window.alert("Name for new credential is empty");
       }
       const startResponse = await fetch("/api/register", { method: "GET" });
       if (!startResponse.ok) {
-        return window.alert("failed to start credential registration");
+        return window.alert("Failed to start credential registration");
       }
       const endResponse = await fetch("/api/register", {
         method: "POST",
@@ -65,12 +40,25 @@ document.addEventListener("DOMContentLoaded", function () {
         }),
       });
       if (!endResponse.ok) {
-        window.alert("failed to end credential registration");
+        window.alert("Failed to end credential registration");
       } else location.reload();
     });
   }
-
   if (document.getElementById("authenticating-msg") !== null) {
-    authenticate().catch(console.error);
+    (async () => {
+      const startResponse = await fetch("/api/authenticate", { method: "GET" });
+      if (!startResponse.ok) {
+        return window.alert("Failed to start authentication");
+      } else if (startResponse.status === 204) return location.reload(); // no user credentials
+      const endResponse = await fetch("/api/authenticate", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(
+          await get(parseRequestOptionsFromJSON(await startResponse.json())),
+        ),
+      });
+      if (!endResponse.ok) return window.alert("Not authenticated");
+      return location.replace("/authenticate"); // client is now logged in
+    })().catch(console.error);
   }
 });
