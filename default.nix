767fce4af4c926d2004a92a8bc85922af098539b
Jared Baur <jaredbaur@fastmail.com>
2022-12-05 08:56:27 -0800

    Python test typing

--- default.nix
+++ default.nix
@@ -1,15 +1,12 @@
 { rustPlatform, llvmPackages_latest, pkg-config, openssl, sqlite, lib, ui, ... }:
-let
-  cargoToml = lib.importTOML ./Cargo.toml;
-  pname = cargoToml.package.name;
-  inherit (cargoToml.package) version;
-in
+let cargoToml = lib.importTOML ./Cargo.toml; in
 rustPlatform.buildRustPackage {
-  inherit pname version;
+  pname = cargoToml.package.name;
+  version = cargoToml.package.version;
   src = ./.;
   buildInputs = [ sqlite openssl ];
   nativeBuildInputs = [ llvmPackages_latest.bintools pkg-config ];
-  ASSETS_DIRECTORY = builtins.toPath ui;
+  ASSETS_DIRECTORY = toString ui;
   RUSTFLAGS = "-C link-arg=-fuse-ld=lld";
   cargoLock.lockFile = ./Cargo.lock;
 }
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-machine.wait_for_unit("webauthn-tiny.service")
-machine.wait_for_open_port(8080)
-machine.fail("curl -v --fail [::1]:8080/authenticate")
-machine.fail("curl -v --fail -u user:wrong_password [::1]:8080/authenticate")
-machine.succeed("curl -v --fail -u user:password [::1]:8080/authenticate")
+m = machine  # type: ignore
+m.wait_for_unit("webauthn-tiny.service")
+m.wait_for_open_port(8080)
+m.fail("curl -v --fail [::1]:8080/authenticate")
+m.fail("curl -v --fail -u user:wrong_password [::1]:8080/authenticate")
+m.succeed("curl -v --fail -u user:password [::1]:8080/authenticate")
