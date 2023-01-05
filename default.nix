90026033aecc564ac8e603905921c3c72f50a746
Jared Baur <jaredbaur@fastmail.com>
2023-01-05 01:35:12 -0800

    Only pull in QEMU if the build requires cross-compilation

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
 { lib, crane, pkgsBuildHost, stdenv, qemu, pkgconfig, openssl, sqlite, ... }:
 let
   toEnvVar = s: lib.replaceStrings [ "-" ] [ "_" ] (lib.toUpper s);
+  isCrossBuild = stdenv.hostPlatform.system != stdenv.buildPlatform.system;
   target = stdenv.hostPlatform.config;
   toolchain = pkgsBuildHost.rust-bin.stable.latest.default.override { targets = [ target ]; };
-  env = {
+  env = (lib.optionalAttrs isCrossBuild {
     "CARGO_TARGET_${toEnvVar target}_LINKER" = "${stdenv.cc.targetPrefix}cc";
     "CARGO_TARGET_${toEnvVar target}_RUNNER" = "qemu-${stdenv.hostPlatform.qemuArch}";
+  }) // {
     CARGO_BUILD_TARGET = target;
     HOST_CC = "${stdenv.cc.nativePrefix}cc";
   };
@@ -13,7 +15,7 @@ in
 (crane.lib.${stdenv.buildPlatform.system}.overrideToolchain toolchain).buildPackage ({
   src = ./.;
   cargoToml = ./Cargo.toml;
-  depsBuildBuild = [ qemu ];
+  depsBuildBuild = lib.optional isCrossBuild qemu;
   nativeBuildInputs = [ toolchain pkgconfig ];
   buildInputs = [ sqlite openssl ];
   ASSETS_DIRECTORY = toString (pkgsBuildHost.callPackage ./ui.nix { });
